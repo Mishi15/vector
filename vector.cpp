7dd35c51290d00be8e5acbb87072ac8b13f3422f
Mishi15 <122174407+Mishi15@users.noreply.github.com>
2023-05-23 12:02:33 +0530

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main (){
+vector<int> vec ;
+for(int i = 0 ; i < 10 ; i++)
+{
+    int temp ;
+    cin >> temp ;
+    vec.push_back(temp);
+}
+for(int i = 0 ; i < 10 ; i++){
+    cout << vec [i] ;
+}
+
+
+
+}
